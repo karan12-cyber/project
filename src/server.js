d2c717543beab788d83f5eab04c76116dbe0f174
utk-1 <utkarsh84477tri@gmail.com>
2026-02-15 19:07:50 +0530

    Add server.js and index.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -14,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ status: "ok", message: "Backend running 🚀" });
+  res.sendFile(path.join(__dirname, "..", "index.html"));
 });
 
 app.use("/api/gym", gymRoutes);
